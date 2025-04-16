013ad65eb2208f001c3cf4aab16139792324f065
mclarke16 <107970544+mrrogers16@users.noreply.github.com>
2025-04-16 15:23:02 -0500

    fixes

--- portfolio-app/src/lib/mongoose.ts
+++ portfolio-app/src/lib/mongoose.ts
@@ -1,16 +1,22 @@
 import mongoose from 'mongoose';
 
-let cached = (global as any).mongoose || { conn: null };
+interface MongooseCache {
+    conn: typeof mongoose | null;
+}
+
+declare global {
+    // Safe declaration so we don't overwrite
+    var mongooseCache: MongooseCache | undefined;
+}
+
+const cached: MongooseCache = global.mongooseCache || { conn: null };
 
 export async function dbConnect() 
 {
-    if (cached.conn) 
-    {
-        return cached.conn;
-    }
+    if (cached.conn) return cached.conn;
 
     cached.conn = await mongoose.connect(process.env.MONGODB_URI!);
-    (global as any).mongoose = cached;
+    global.mongooseCache = cached;
 
     return cached.conn;
 }
