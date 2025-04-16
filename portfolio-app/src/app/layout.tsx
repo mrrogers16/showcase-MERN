8dda5e222033f4d9635829da62ae1444e451c940
mclarke16 <107970544+mrrogers16@users.noreply.github.com>
2025-04-16 15:04:58 -0500

    fix

--- portfolio-app/src/app/layout.tsx
+++ portfolio-app/src/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from "next";
-import { ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -26,9 +33,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+
+          </header>
           {children}
         </body>
       </html>
--- portfolio-app/src/middleware.ts
+++ portfolio-app/src/middleware.ts
@@ -1,13 +1,12 @@
-// middleware.ts
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
-// Tell Next which routes to apply the middleware to
 export const config = {
-    matcher: [
-        "/((?!.*\\..*|_next).*)", // everything except static files
-        "/",                      // homepage
-        "/(api|trpc)(.*)"         // APIs
-    ],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
